5de23b94ff79dc10055b72f92a64dd77d0138e9e
Zain Kergaye <zain4utah@gmail.com>
2023-11-12 17:31:50 -0700

    Revert "Added basic components plus used routes"
    This reverts commit 1680ca4c7a01d202db16dff59d849ea997fde193.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-<!-- Importing routes here is critical for navigation -->
-
-import { NavigationComponent } from './navigation/navigation.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { MenuComponent } from './menu/menu.component';
-import { ReviewComponent } from './review/review.component';
-
-
-const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'menu', component: MenuComponent },
-  { path: 'review', component: ReviewComponent},
-
-];
-
-
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
