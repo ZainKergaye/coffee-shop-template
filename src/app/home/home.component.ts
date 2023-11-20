c9117686852c7789009ca2ea7139884364f53852
Zain Kergaye <zain4utah@gmail.com>
2023-11-20 14:04:38 -0700

    Adding slider to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { interval } from 'rxjs';
+import { take } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  @ViewChild('IMGSlider') IMGSlider: ElementRef | undefined;
 
+
+  images: String[] = [
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png',
+    'assets/Placeholder2.png'
+  ];
+
+  currentSlide = 0;
+
+  ngOnInit() {
+    this.startAutoScroll();
+  }
+
+  startAutoScroll() {
+    interval(3000) // Adjust the interval (in milliseconds) between slides
+      .pipe(take(this.images.length))
+      .subscribe(() => {
+        this.nextSlide();
+      });
+  }
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+    if (this.IMGSlider) {
+      this.IMGSlider.nativeElement.style.transform = `translateX(${-this.currentSlide * 100}%)`;
+    }
+  }
 }
